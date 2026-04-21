61ef0986dc3dd929b326c8709b6a277b8f41bdac
0xMayurrr <mayur@example.com>
2026-04-21 22:49:08 +0530

    fix: remove react-native-worklets conflict causing Android production crash

--- index.ts
+++ index.ts
@@ -1,6 +1,5 @@
 import 'react-native-get-random-values';
 import '@ethersproject/shims';
-import 'react-native-reanimated';
 import { registerRootComponent } from 'expo';
 import App from './App';
 
